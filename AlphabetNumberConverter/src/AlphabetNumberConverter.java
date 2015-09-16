4e219313672dcda87ad657bd799f88527b8a8b15
AdrielTan <a0125283@u.nus.edu>
2015-09-16 19:57:26 +0800

    added AlphabetNumberConverter()

--- AlphabetNumberConverter/src/AlphabetNumberConverter.java
+++ AlphabetNumberConverter/src/AlphabetNumberConverter.java
@@ -1,4 +1,14 @@
+import java.io.File;
+import java.io.IOException;
 
 public class AlphabetNumberConverter {
+	
+	public AlphabetNumberConverter() throws IOException{
+		File newFile = new File("newfile.txt");
+		startFile(newFile);
+	}
 
+	private void startFile(File newFile) throws IOException{
+		newFile.createNewFile();		
+	}
 }
